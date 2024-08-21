84d3f48a77ad39481f2db13f43846e262f45f888
aqua <aquarell.dev@gmail.com>
2024-08-22 00:25:54 +0300

    fix: removed useless indicators

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -48,7 +48,7 @@
 
     "pulseaudio" = {
         # scroll-step = 1; # %, can be a float
-        reverse-scrolling = 1;
+        reverse-scrolling = 0;
         format = "{volume}% {icon} {format_source}";
         format-bluetooth = "{volume}% {icon} {format_source}";
         format-bluetooth-muted = " {icon} {format_source}";
