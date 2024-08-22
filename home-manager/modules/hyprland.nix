cc4f2be5c3461d7bb9a58d48f45158c4c8a0aa3f
aqua <aquarell.dev@gmail.com>
2024-08-22 13:49:04 +0300

    fix: revert scrolling behaviour on workspace swipe

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -88,7 +88,7 @@
       gestures = {
         workspace_swipe = true;
         workspace_swipe_fingers = 3;
-        workspace_swipe_invert = false;
+        workspace_swipe_invert = true;
         workspace_swipe_distance = 200;
         workspace_swipe_forever = true;
       };
